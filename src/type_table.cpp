4750aa27f865e9d576f843c2f2f6f5a192eac6a5
Bence Nagy <bence657@gmail.com>
2019-04-24 15:45:32 +0200

    Fixed type numbers

--- src/type_table.cpp
+++ src/type_table.cpp
@@ -3,8 +3,8 @@
 
 type_table_t::type_table_t() {
     type_map = std::unordered_map<int, type_descriptor_t>();
-    insert(0, (type_descriptor_t){"char", 1});
-    insert(1, (type_descriptor_t){"int", 2});
+    insert(0, (type_descriptor_t){"int", 2});
+    insert(1, (type_descriptor_t){"char", 1});
     insert(2, (type_descriptor_t){"long", 4});
 }
 
