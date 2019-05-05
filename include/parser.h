9bada1639436fe590c98fb92f79dd89b7d0ece02
Bence Nagy <bence657@gmail.com>
2019-05-05 14:23:18 +0200

    Removed old and unused code

--- include/parser.h
+++ include/parser.h
@@ -66,70 +66,6 @@ struct greater_eq_binop_t;
 // will search for the component terminals, and once a production is identified, call the semantic
 // function of that production
 
-// Tags for productions, mostly inherited from tag_t
-enum class prod_tags_t : int {
-
-        /* Terminal types, inherited from tag_t */
-
-        // Keywords
-        IF = (int) lex::tag_t::IF,
-        RETURN = (int) lex::tag_t::RETURN,
-
-        // Other
-        ID = (int) lex::tag_t::ID,
-        INT_LITERAL = (int) lex::tag_t::INT_LITERAL,
-        STRING_LITERAL = (int) lex::tag_t::STRING_LITERAL,
-
-        // Operators
-        PLUS = (int) lex::tag_t::PLUS,
-        MINUS = (int) lex::tag_t::MINUS,
-        EQUALS = (int) lex::tag_t::EQUALS,
-        NOT_EQUALS = (int) lex::tag_t::NOT_EQUALS,
-        ASSIGNMENT = (int) lex::tag_t::ASSIGNMENT,
-        
-        // Special characters
-        OPEN_PAREN = (int) lex::tag_t::OPEN_PAREN,
-        CLOSED_PAREN = (int) lex::tag_t::CLOSED_PAREN,
-        OPEN_BRACE = (int) lex::tag_t::OPEN_BRACE,
-        CLOSED_BRACE = (int) lex::tag_t::CLOSED_BRACE,
-        SEMI_COLON = (int) lex::tag_t::SEMI_COLON,
-        
-        // Probably remove these two
-        eof = (int) lex::tag_t::eof,
-        UNKNOWN = (int) lex::tag_t::UNKNOWN,
-
-        /* Non-terminals newly added */
-
-        PROGRAM,
-        DECLS,
-        DECL,
-        VAR_DECL,
-        FUNC_DECL,
-        STMT,
-        STMTS,
-        BLOCK_STMT,
-        EXPR,
-        ARITHOP,
-        RELOP,
-        TERM,
-        // Empty string
-        EMPTY
-}; 
-
-// Hash function for enum classes
-struct enum_class_hash_t {
-    template <typename T>
-    std::size_t operator()(T t) const {
-        return static_cast<std::size_t>(t);
-    }
-};
-
-// Struct for productions
-struct prod_t {
-    std::vector<prod_tags_t> rule;
-    //std::function<ast_node_t*()> semantics;
-};
-
 class syntax_error : public std::exception {
 private:
     std::string msg;
@@ -142,8 +78,6 @@ public:
 
 class parser_t {
 private:
-    // Map of all productions, might be redundant
-    std::unordered_map<prod_tags_t, std::vector<prod_t>, enum_class_hash_t> productions;
     
     // The lexical analyzer used by the parser
     lex::lexer *lexical_analyzer;
--- include/tokens.h
+++ include/tokens.h
@@ -5,19 +5,6 @@
 
 namespace lex {
 
-    /* Enum for token group identifiers */
-    enum class type_t : int {
-        
-        GENERAL,
-        KEYWORD,
-        IDENTIFIER,
-        ASSIGNMENT,
-        INT_LITERAL,
-        STRING_LITERAL,
-        ARITHMETIC_OPERATOR,
-        RELATIONAL_OPERATOR
-    };
-
     /* Enum for specific tokens */
     enum class tag_t : int {
         
@@ -93,8 +80,6 @@ namespace lex {
         token(tag_t _tag) : tag(_tag) { }
         int line_number;
         int column_number;
-
-        virtual type_t type() { return type_t::GENERAL; }
     };
 
     struct id_token : token {
@@ -102,20 +87,16 @@ namespace lex {
         id_token(const std::string& _lexeme) : token(tag_t::ID) { lexeme = _lexeme; }
         id_token(const std::string&& _lexeme) : token(tag_t::ID) { lexeme = _lexeme; }
 
-        virtual type_t type() override { return type_t::IDENTIFIER; }
     };
 
     struct keyword_token : token {
         keyword_token(tag_t _tag) : token(_tag) { }
-
-        virtual type_t type() override { return type_t::KEYWORD; }
     };
 
     struct int_literal_token : token {
         int value;
         int_literal_token(int _value) : token(tag_t::INT_LITERAL), value(_value) {}
 
-        virtual type_t type() override { return type_t::INT_LITERAL; }
     };
 
     struct str_literal_token : token {
@@ -123,19 +104,6 @@ namespace lex {
         str_literal_token(const std::string& _value) : token(tag_t::STRING_LITERAL) { value = _value; }
         str_literal_token(const std::string&& _value) : token(tag_t::STRING_LITERAL) { value = _value; }
 
-        virtual type_t type() override { return type_t::STRING_LITERAL; }
-    };
-
-    struct arithop_token : token {
-        arithop_token(tag_t _tag) : token(_tag) { }
-
-        virtual type_t type() override { return type_t::ARITHMETIC_OPERATOR; }
-    };
-
-    struct relop_token : token {
-        relop_token(tag_t _tag) : token(_tag) { }
-
-        virtual type_t type() override { return type_t::RELATIONAL_OPERATOR; }
     };
 
 }
